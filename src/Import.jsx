da3a3990ed0777fa7ac5f670617dba576c010a0e
Jair Romero Peña <jair@equipo3.com>
2023-06-05 15:54:25 -0600

    Api_Final

---
+++ src/Import.jsx
@@ -0,0 +1,86 @@
+
+
+const images = [
+    {
+        src: "https://http.cat/203",
+        Card: '203'
+    },
+    {
+        src: "https://http.cat/203",
+        Card: '203'
+    },
+    {
+        src: "https://http.cat/303",
+        Card: '303'
+    },
+    {
+        src: "https://http.cat/303",
+        Card: '303'
+    },
+    {
+        src: "https://http.cat/308",
+        Card: '308'
+    },
+    {
+        src: "https://http.cat/308",
+        Card: '308'
+    },
+    {
+        src: "https://http.cat/402",
+        Card: '402'
+    },
+    {
+        src: "https://http.cat/402",
+        Card: '402'
+    },
+    {
+        src: "https://http.cat/410",
+        Card: '410'
+    },
+    {
+        src: "https://http.cat/410",
+        Card: '410'
+    },
+    {
+        src: "https://http.cat/423",
+        Card: '423'
+    },
+    {
+        src: "https://http.cat/423",
+        Card: '423'
+    },
+    {
+        src: "https://http.cat/497",
+        Card: '497'
+    },
+    {
+        src: "https://http.cat/497",
+        Card: '497'
+    },
+    {
+        src: "https://http.cat/502",
+        Card: '502'
+    },
+    {
+        src: "https://http.cat/502",
+        Card: '502'
+    },
+    {
+        src: "https://http.cat/506",
+        Card: '506'
+    },
+    {
+        src: "https://http.cat/506",
+        Card: '506'
+    },
+    {
+        src: "https://http.cat/525",
+        Card: '525'
+    },
+    {
+        src: "https://http.cat/525",
+        Card: '525'
+    },
+];
+
+export {images}
